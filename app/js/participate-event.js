4301f8e4991fa304a94e5a8f73c64131abfd526b
Agnès Cappello <senga.ds@gmail.com>
2024-12-20 13:04:16 +0100

    qrcode scanner

---
+++ app/js/participate-event.js
@@ -0,0 +1,38 @@
+console.log("hello from parcitipate event");
+
+function domReady(fn) {
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+domReady(function () {
+  const qr = document.getElementById("qr-result");
+  let lastResult;
+  let countResults = 0;
+
+  function onScanSuccess(decodeText, decodeResult) {
+    if (decodeText !== lastResult) {
+      lastResult = decodeText;
+      countResults++;
+      lastResult = decodeResult;
+      alert("qr code is: " + decodeText, decodeResult);
+      //qr.innerHTML = `<div>${decodeText}</div>`;
+      qr.textContent = `you scan ${countResults} : ${decodeText}`;
+    }
+  }
+
+  const htmlScanner = new Html5QrcodeScanner("qr-reader", {
+    fps: 30,
+    qrbox: 250,
+    imageSmoothing: false,
+    createCanvasFromImage: true,
+    qrtype: "IMAGE_ONLY",
+  });
+  htmlScanner.render(onScanSuccess);
+});
